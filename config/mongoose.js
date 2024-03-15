744c3ebfd5d40d445f4b8276753150f5100f89ba
sanket622 <sanketkumar0068@gmail.com>
2024-03-15 19:00:54 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,29 @@
+// IMPORTING PACKAGE
+const mongoose = require("mongoose");
+
+
+// MAKING CONNECTION 
+const DB = mongoose.connect('mongodb://localhost:27017/csvUploads', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// Handle connection status
+DB.then(() => {
+    console.log('Connection successful!');
+}).catch((err) => {
+    console.log('No connection:', err);
+});
+
+
+// Setting it to db
+const db = mongoose.connection;
+
+
+// CHECKING CONNECTION
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+module.exports = db;
